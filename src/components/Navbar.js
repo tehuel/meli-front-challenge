5322e3ab9454496e09404b770369b8bf109cec87
Tehuel <torresbaldi.tehuel@gmail.com>
2020-05-15 16:38:14 -0300

    handle navigation click

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,19 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
 
 import logo from "../assets/img/logo.svg";
 import "../assets/styles/Navbar.scss";
 
 export default function Navbar() {
+  const [query, setQuery] = useState('');
+  let history = useHistory();
+
+  function handleClick(e) {
+    e.preventDefault();
+    console.log('Busqueda iniciada', query);
+    history.push("/items?search=" + query);
+  }
+
   return (
     <nav className="main-navbar">
       <div className="container navbar-container">
@@ -14,8 +23,8 @@ export default function Navbar() {
           </Link>
         </div>
         <div className="search-form">
-          <input type="text" id="search-query" name="search-query" required/>
-          <button>Buscar</button>
+          <input type="text" id="search-query" name="search-query" required value={query} onChange={e => setQuery(e.target.value)}/>
+          <button onClick={handleClick}>Buscar</button>
         </div>
       </div>
     </nav>

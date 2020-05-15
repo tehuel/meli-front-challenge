e62c0840ea0f537eac86e6fcfc75895d65880fc2
Tehuel <torresbaldi.tehuel@gmail.com>
2020-05-15 16:08:45 -0300

    uses SCSS on navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
+
 import logo from "../assets/img/logo.svg";
+import "../assets/styles/Navbar.scss";
 
 export default function Navbar() {
   return (

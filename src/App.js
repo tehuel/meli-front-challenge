14bded90ed5e9533ea6e68e72d81112d7c330c44
Tehuel <torresbaldi.tehuel@gmail.com>
2020-05-15 15:50:19 -0300

    fix: remove unused import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 
 import SingleItem from "./pages/SingleItem";
 import ItemList from "./pages/ItemList";

7b359828ec970b456926439a566088fd72dc8d24
Tehuel <torresbaldi.tehuel@gmail.com>
2020-05-16 16:21:12 -0300

    format price

--- src/components/ItemComponent.js
+++ src/components/ItemComponent.js
@@ -1,13 +1,24 @@
 import React from "react";
 import {Link} from "react-router-dom";
 
+function formatPrice(price) {
+  const formatter = new Intl.NumberFormat('es-AR', {
+    style: 'currency',
+    currency: price.currency,
+  });
+  const floatValue = parseFloat(`${price.amount}.${price.decimals}`);
+  const formattedPrice = formatter.format(floatValue);
+  return formattedPrice;
+}
+
 export default function ItemComponent({ item }) {
+  const formattedItemPrice = formatPrice(item.price);
   return <div className="item">
     <Link to={`/items/${item.id}`}>
       <img src={item.picture} alt={item.title}/>
     </Link>
     <div className="content">
-      <p>$ 999</p>
+      <p>{formattedItemPrice}</p>
       <h2>{item.title}</h2>
     </div>
   </div>;

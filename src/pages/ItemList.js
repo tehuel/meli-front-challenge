2141497052138d4556dfc7bbdfe6ebed1f8d6add
Tehuel <torresbaldi.tehuel@gmail.com>
2020-05-15 23:47:04 -0300

    get search data in results page uses custom query sync hook

--- src/pages/ItemList.js
+++ src/pages/ItemList.js
@@ -1,37 +1,54 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { Link } from "react-router-dom";
 import '../assets/styles/search.css';
 import '../assets/styles/results.css';
 import Navbar from "../components/Navbar";
+import {useQueryLocationSync} from "../hooks/UseQueryLocationSync";
 
 export default function ItemList() {
+  // const [error, setError] = useState(null);
+  // const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [query] = useQueryLocationSync();
+
+  useEffect(() => {
+    console.log("aca ejecuto el request a la API", query);
+    const fetchSearch = async () => {
+      if (query) {
+        const searchUrl = encodeURI('/api/items?search=' + query)
+        const searchResponse = await fetch(searchUrl);
+        const searchData = await searchResponse.json();
+        setItems(searchData.items)
+        setCategories(searchData.categories)
+      }
+    };
+    fetchSearch();
+  }, [query])
+
   return (
     <>
       <Navbar/>
       <div>
         <div className="container">
           <div className="breadcrumb">
-            aaa | aaa | aaa
+            {JSON.stringify(categories)}
           </div>
           <div className="search-results">
-            <div className="item">
-              <Link to="/items/1">
-                <img src="https://picsum.photos/300" alt=""/>
-              </Link>
-              <div className="content">
-                <p>$ 999</p>
-                <h2>Nombre del producto</h2>
-              </div>
-            </div>
-            <div className="item">
-              <Link to="/items/2">
-                <img src="https://picsum.photos/300" alt=""/>
-              </Link>
-              <div className="content">
-                <p>$ 999</p>
-                <h2>Nombre del producto</h2>
-              </div>
-            </div>
+            {
+              items.map(item => { return (
+                <div className="item" key={item.id}>
+                  <Link to={`/items/${item.id}`}>
+                    <img src={item.picture} alt={item.title}/>
+                  </Link>
+                  <div className="content">
+                    <p>$ 999</p>
+                    <h2>{item.title}</h2>
+                  </div>
+                </div>
+
+              )})
+            }
           </div>
         </div>
       </div>

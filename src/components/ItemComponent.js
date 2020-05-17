f0ab562c2f6582db62547d6c5e74498b855f311f
Tehuel <torresbaldi.tehuel@gmail.com>
2020-05-17 15:02:05 -0300

    product list details

--- src/components/ItemComponent.js
+++ src/components/ItemComponent.js
@@ -12,13 +12,19 @@ function formatPrice(price) {
 
 export default function ItemComponent({ item }) {
   const formattedItemPrice = formatPrice(item.price);
-  return <div className="item">
-    <Link to={`/items/${item.id}`}>
-      <img src={item.picture} alt={item.title}/>
-    </Link>
-    <div className="content">
-      <p>{formattedItemPrice}</p>
-      <h2>{item.title}</h2>
-    </div>
-  </div>;
+  return (
+      <div className="item">
+        <img src={item.picture} alt={item.title}/>
+        <div className="item-content">
+          <Link to={`/items/${item.id}`} className="stretched-link">
+            <div className="product-information">
+              <p className="product-price">{formattedItemPrice}</p>
+              <p className="product-shipping">{item.free_shipping ? 'Envio Gratis' : ''}</p>
+              <p className="product-condition">{item.condition === 'new' ? 'Nuevo' : 'Usado' }</p>
+            </div>
+          <h2 className="product-title">{item.title}</h2>
+          </Link>
+        </div>
+      </div>
+  );
 }

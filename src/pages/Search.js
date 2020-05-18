a7c2665fce8ff1fb7210dfb532eb25910dd37bc2
Tehuel <torresbaldi.tehuel@gmail.com>
2020-05-18 01:26:18 -0300

    refactor home style

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -5,8 +5,8 @@ import "../assets/styles/style.scss";
 export default function Search() {
   return (
     <div className="container">
-      <div className="page-container">
-        <p>Hola.</p>
+      <div className="page-container home">
+        <p>Hola, bienvenido.</p>
       </div>
     </div>
   );
